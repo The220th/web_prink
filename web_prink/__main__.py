a60d5b117bd8b0e7c902220798a74dc81019462d
Your Name <you@example.com>
2024-05-31 12:43:32 +0300

    V0.2.0

--- web_prink/__main__.py
+++ web_prink/__main__.py
@@ -7,7 +7,7 @@ base_dir = os.path.dirname(__file__)
 sys.path.insert(0, base_dir)
 
 
-from . import main
+from .main import main
 
 
 if __name__ == "__main__":
--- web_prink/main.py
+++ web_prink/main.py
@@ -9,37 +9,60 @@ import sys
 base_dir = os.path.dirname(__file__)
 sys.path.insert(0, base_dir)
 
-from . import __version__
-
+import time
+import json
 import gradio as gr
 
+from . import __version__
+from . import GL
+from . import print_pdf
+from . import scan_file
 
-def print_pdf(input_file):
-    print(input_file.name)
-    res = "Wait until printing will be finished."
+
+def print_pdf_main(input_file):
+    print(f"Will be printed \"{input_file.name}\". ")
+    res = print_pdf(input_file.name)
     return res
 
 
-def scan_file(pass_text):
-    print(pass_text)
-    file = "testfile.mp4"
-    return file
+def scan_file_main(format_text: str):
+    res_file = scan_file(200, format_text)
+    return res_file
+
+
+def parse_commands():
+    json_path = os.path.join(GL.root_path, "execs.json")
+    with open(json_path, "r", encoding="utf-8") as fd:
+        s = fd.read()
+    d = json.loads(s)
+    GL.print_command_raw = d["print_command"]
+    GL.scan_command_raw = d["scan_command"]
+    if GL.print_command_raw == "" or GL.scan_command_raw == "":
+        print(f"Please define files in file \"{json_path}\". ")
+        exit(1)
 
 
 def main():
+    print("\n\n\tDont forget run it as root, if needed.\n")
+    time.sleep(3.0)
+
+    GL.root_path = os.path.dirname(__file__)
+
+    parse_commands()
+
     with gr.Blocks() as demo:
 
         with gr.Tab("Print"):
             input1 = gr.File(label="Choose pdf")
             output1 = gr.Textbox(label="Output")
             submit1 = gr.Button("Print pdf")
-            submit1.click(print_pdf, inputs=input1, outputs=output1)
+            submit1.click(print_pdf_main, inputs=[input1], outputs=[output1])
 
         with gr.Tab("Scan"):
             input2 = gr.Radio(["tiff", "png", "pdf"], label="Output format")
             output2 = gr.File(label="Download scanned file. ")
             submit2 = gr.Button("Scan")
-            submit2.click(scan_file, inputs=input2, outputs=output2)
+            submit2.click(scan_file_main, inputs=[input2], outputs=[output2])
 
     demo.launch()
 
